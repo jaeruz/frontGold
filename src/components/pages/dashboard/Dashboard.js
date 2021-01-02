0b51acea74d1633766344ed6cb108673c8227775
Jeffrey Esma <abd.kgd@gmail.com>
2021-01-03 00:26:28 +0800

    Dashboard Update

--- src/components/pages/dashboard/Dashboard.js
+++ src/components/pages/dashboard/Dashboard.js
@@ -1,15 +1,14 @@
 import React, { useState } from 'react';
-import { Container, Form } from 'react-bootstrap';
+import { Container, Form, Row, Col } from 'react-bootstrap';
 import DashboardChart from './DashboardChart';
 import DashboardTable from './DashboardTable';
 
 function Dashboard() {
 	const [chartOpen, setChartOpen] = useState(true);
 	const handleChange = (e) => {
-		let renderView = e.target.value;
 		// console.log(e.target.value);
 		// console.log(e.target.id);
-		if (renderView === 'CHART') {
+		if (e.target.value === 'CHART') {
 			setChartOpen(true);
 		} else {
 			setChartOpen(false);
@@ -18,11 +17,19 @@ function Dashboard() {
 	return (
 		<Container className="dashboard-container">
 			<Form.Group controlId="dashboard-view-as">
-				<Form.Label>View as:</Form.Label>
-				<Form.Control as="select" onChange={handleChange} className="form-caps">
-					<option>CHART</option>
-					<option>TABLE</option>
-				</Form.Control>
+				<div class="dashboard-view-wrapper">
+					<Form.Label>View as:</Form.Label>
+					<div class="dashboard-select-wrapper">
+						<Form.Control
+							as="select"
+							onChange={handleChange}
+							className="form-caps"
+						>
+							<option>CHART</option>
+							<option>TABLE</option>
+						</Form.Control>
+					</div>
+				</div>
 			</Form.Group>
 			<div class="dashboard-components">
 				{chartOpen ? <DashboardChart /> : <DashboardTable />}
--- src/components/pages/dashboard/DashboardChart.js
+++ src/components/pages/dashboard/DashboardChart.js
@@ -1,7 +1,201 @@
 import React from 'react';
+import { Bar, Line, Pie, Doughnut } from 'react-chartjs-2';
+import { DataSample1, DataSample2, DataSample3, DataSample4 } from './Data';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function DashboardChart() {
-	return <div>Hello Charts</div>;
+	return (
+		<Container>
+			<Row md={1} lg={2}>
+				<Col>
+					<div class="chart-wrapper">
+						<Bar
+							data={DataSample1}
+							options={{
+								maintainAspectRatio: false,
+
+								scales: {
+									xAxes: [
+										{
+											gridLines: {
+												offsetGridLines: true,
+											},
+										},
+									],
+									yAxes: [
+										{
+											ticks: {
+												beginAtZero: true,
+											},
+										},
+									],
+								},
+								legend: {
+									display: true,
+									labels: {
+										fontColor: '#1e3c4c',
+										boxWidth: 40,
+									},
+									position: 'top',
+								},
+								title: {
+									display: true,
+									text: 'Data Sample 1',
+									fontSize: 16,
+									fontColor: '#1e3c4c',
+								},
+								layout: {
+									padding: {
+										left: 30,
+										right: 30,
+										bottom: 10,
+										top: 0,
+									},
+								},
+								tooltips: {
+									enabled: true,
+								},
+							}}
+						/>
+					</div>
+				</Col>
+				<Col>
+					<div class="chart-wrapper">
+						<Doughnut
+							data={DataSample2}
+							options={{
+								maintainAspectRatio: false,
+								scales: {
+									yAxes: [
+										{
+											ticks: {
+												beginAtZero: true,
+											},
+										},
+									],
+								},
+								legend: {
+									display: true,
+									labels: {
+										fontColor: '#1e3c4c',
+										boxWidth: 40,
+									},
+									position: 'right',
+								},
+								title: {
+									display: true,
+									text: 'Data Sample 2',
+									fontSize: 16,
+									fontColor: '#1e3c4c',
+								},
+								layout: {
+									padding: {
+										left: 30,
+										right: 30,
+										bottom: 10,
+										top: 0,
+									},
+								},
+								tooltips: {
+									enabled: true,
+								},
+							}}
+						/>
+					</div>
+				</Col>
+			</Row>
+			<Row md={1} lg={2}>
+				<Col>
+					<div class="chart-wrapper">
+						<Pie
+							data={DataSample3}
+							options={{
+								maintainAspectRatio: false,
+								scales: {
+									yAxes: [
+										{
+											ticks: {
+												beginAtZero: true,
+											},
+										},
+									],
+								},
+								legend: {
+									display: true,
+									labels: {
+										fontColor: '#1e3c4c',
+										boxWidth: 40,
+									},
+									position: 'bottom',
+								},
+								title: {
+									display: true,
+									text: 'Data Sample 3',
+									fontSize: 16,
+									fontColor: '#1e3c4c',
+								},
+								layout: {
+									padding: {
+										left: 30,
+										right: 30,
+										bottom: 10,
+										top: 0,
+									},
+								},
+								tooltips: {
+									enabled: true,
+								},
+							}}
+						/>
+					</div>
+				</Col>
+				<Col>
+					<div class="chart-wrapper">
+						<Line
+							data={DataSample4}
+							options={{
+								maintainAspectRatio: false,
+								scales: {
+									yAxes: [
+										{
+											ticks: {
+												beginAtZero: true,
+											},
+										},
+									],
+								},
+								legend: {
+									display: true,
+									labels: {
+										fontColor: '#1e3c4c',
+										boxWidth: 40,
+									},
+									position: 'top',
+								},
+								title: {
+									display: true,
+									text: 'Data Sample 4',
+									fontSize: 16,
+									fontColor: '#1e3c4c',
+								},
+								layout: {
+									padding: {
+										left: 30,
+										right: 30,
+										bottom: 10,
+										top: 0,
+									},
+								},
+								tooltips: {
+									enabled: true,
+								},
+							}}
+						/>
+					</div>
+				</Col>
+			</Row>
+		</Container>
+	);
 }
 
 export default DashboardChart;
